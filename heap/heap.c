03c87490e4fcae595520ffeac41d5283053b71ae
Joseph Schilz <joseph@schilz.org>
2018-09-08 05:50:37 +0000

    Stub out heap methods.

---
+++ heap/heap.c
@@ -0,0 +1,67 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "heap.h"
+
+
+struct Item_t {
+    int key;
+    void* value;
+};
+typedef struct Item_t Item_t;
+
+
+struct Heap_t {
+    size_t size;
+    Item_t** storage;
+    size_t next_free;
+};
+typedef struct Heap_t Heap_t;
+
+void payload_free_function(ItemPayload_t payload)
+{
+    // Stub method.
+}
+
+Item_t* AllocateItem(int key,
+                     ItemPayload_t payload,
+                     ItemPayloadFreeFnPtr payload_free_function)
+{
+    return NULL;
+}
+
+int FreeItem(Item_t* item)
+{
+    return 0;
+}
+
+Heap_t* AllocateHeap(size_t size)
+{
+    return NULL;
+}
+
+int Freeheap(Heap_t* heap)
+{
+    return 0;
+}
+
+int PushHeap(Heap_t* heap, Item_t* item)
+{
+    return 0;
+}
+
+Item_t* PopHeap(Heap_t* heap)
+{
+    return NULL;
+}
+
+Item_t* PeekHeap(Heap_t* heap)
+{
+    return NULL;
+}
+
+int main()
+{
+    printf("Hello world!");
+
+    return 0;
+}
