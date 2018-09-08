e904c33fb5ea7d8f3aa57d7f09c8c914b8714546
Joseph Schilz <joseph@schilz.org>
2018-09-08 08:01:07 +0000

    Add item init test.

--- heap/heap_test.cpp
+++ heap/heap_test.cpp
@@ -1,6 +1,18 @@
 #include "heap.c"
 #include "gtest/gtest.h"
 
+TEST(ItemTest, ItemInit)
+{
+    ItemPayload_t payload = (ItemPayload_t)malloc(sizeof(int));
+    *(int *)payload = 5;
+
+    Item_t* item = AllocateItem(4, payload, NULL);
+
+    ASSERT_NE(item, nullptr);
+    ASSERT_EQ(item->key, 4);
+    ASSERT_EQ(*(int *)item->payload, 5);
+}
+
 TEST(HeapTest, HeapInit)
 {
     Heap_t* heap = AllocateHeap(12);
