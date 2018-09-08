39e9c9e62c4ee3f92a95a9fce8c32c648af9006a
Joseph Schilz <joseph@schilz.org>
2018-09-08 04:06:48 +0000

    Remove cruft.

--- heap/heap.py
+++ heap/heap.py
@@ -1,24 +1,3 @@
-"""
-Basic
-find-max [or find-min]: find a maximum item of a max-heap, or a minimum item of a min-heap, respectively (a.k.a. peek)
-insert: adding a new key to the heap (a.k.a., push[3])
-extract-max [or extract-min]: returns the node of maximum value from a max heap [or minimum value from a min heap] after removing it from the heap (a.k.a., pop[4])
-delete-max [or delete-min]: removing the root node of a max heap [or min heap], respectively
-replace: pop root and push a new key. More efficient than pop followed by push, since only need to balance once, not twice, and appropriate for fixed-size heaps.[5]
-Creation
-create-heap: create an empty heap
-heapify: create a heap out of given array of elements
-merge (union): joining two heaps to form a valid new heap containing all the elements of both, preserving the original heaps.
-meld: joining two heaps to form a valid new heap containing all the elements of both, destroying the original heaps.
-Inspection
-size: return the number of items in the heap.
-is-empty: return true if the heap is empty, false otherwise.
-Internal
-increase-key or decrease-key: updating a key within a max- or min-heap, respectively
-delete: delete an arbitrary node (followed by moving last node and sifting to maintain heap)
-sift-up: move a node up in the tree, as long as needed; used to restore heap condition after insertion. Called "sift" because node moves up the tree until it reaches the correct level, as in a sieve.
-sift-down: move a node down in the tree, similar to sift-up; used to restore heap condition after deletion or replacement.
-"""
 
 class Item(object):
     
